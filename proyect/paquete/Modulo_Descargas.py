caed4f4db3df1e375ec324fa828f744d02adbff9
Daren_Hein <darenhein@hotail.com>
2023-10-05 22:36:31 -0600

    hola

--- proyect/paquete/Modulo_Descargas.py
+++ proyect/paquete/Modulo_Descargas.py
@@ -1,6 +1,7 @@
 import os 
 def descargas():
     try : 
+        lista = []
         usuario = os.getlogin()
         ruta = "/home"
         nueva_ruta = os.path.join(ruta,usuario)
@@ -11,7 +12,13 @@ def descargas():
                     ruta_final = os.path.join(nueva_ruta,directorios)
         print("ruta actual -> " , ruta_final)
         #todo ahora vamos a listar todo lo que tenga el directorios
-        
+        for elementos in os.listdir(ruta_final):
+            archivo = str(elementos)
+            if archivo.endswith(".jpeg"):
+                archivo = os.path.abspath(archivo)
+                lista.append(archivo)
+        for elementos in lista : 
+            print(elementos)
         pass 
 
     except OSError : 
